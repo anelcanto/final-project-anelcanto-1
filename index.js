26176e34636b34a65973ba0b6452e6bcf95a5fe9
Anel Canto <ac@anelcanto.com>
2023-05-19 23:01:03 -1000

    Fix conneciton issues and secure credentials

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const express = require("express")
-const mongoose = require("mongoose")
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const pass = process.env.MONGO_PASS;
+const user = process.env.MONGO_USER;
+const uri = "mongodb+srv://"+user+":"+pass+"cluster0.61s1myb.mongodb.net/?appName=mongosh+1.9.0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-const app = express()
-const PORT = process.env.PORT || 3000
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-// Connect to MongoDB database
-mongoose
-	.connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
-	.then(() => {
-		const app = express()
-
-		app.listen(PORT, () => {
-			console.log("Server has started!")
-		})
-	})
