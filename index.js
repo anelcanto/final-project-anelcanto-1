5e95d7ab28ed9b1a18e944ce96f72568c00286d5
Anel Canto <ac@anelcanto.com>
2023-05-19 22:08:25 -1000

    Add Mongoose

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-	console.log("Server has started!")
-})
+// Connect to MongoDB database
+mongoose
+	.connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
+	.then(() => {
+		const app = express()
+
+		app.listen(PORT, () => {
+			console.log("Server has started!")
+		})
+	})
