6d10f6dcb520b6d3be89f042b915c8490be129db
Anel Canto <ac@anelcanto.com>
2023-05-20 01:54:48 -1000

    Create first route

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ config(); // config the dotenv environment using biult in config method
 const app = express();
 const uri = process.env.MONGO_URI;
 
+// connect to mongodb
 mongoose.connect(uri, { useNewUrlParser: true })
-console.log("uri: ", uri);
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
+
+
+// set up express
+app.use(express.json()) // allow us to parse json
 app.listen(3000, () => console.log('server started'))
