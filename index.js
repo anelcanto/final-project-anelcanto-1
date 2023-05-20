8f37fd79370422c03d3f37ab18427140df2378b9
Anel Canto <ac@anelcanto.com>
2023-05-20 02:02:56 -1000

    Add all basic endpoints for tasks

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// followinr tutorial https://dev.to/beznet/build-a-rest-api-with-node-express-mongodb-4ho4
 import mongoose from 'mongoose';
 import express from 'express'
 import { config } from 'dotenv';
+import tasksRouter from './routes/tasks.js'
 
 config(); // config the dotenv environment using biult in config method
 const app = express();
@@ -12,7 +14,12 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to database'))
 
-
 // set up express
 app.use(express.json()) // allow us to parse json
-app.listen(3000, () => console.log('server started'))
+
+// set up routes
+app.use('/tasks', tasksRouter)
+
+// start server
+app.listen(3005, () => console.log('server started'))
+
---
+++ routes/tasks.js
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// Define your routes and middleware here
+
+
+router.route('/')
+    // GET all tasks endpoint
+    .get((req, res) => {
+        res.send('Hello World!')
+    })
+    // POST a new task endpoint
+    .post((req, res) => {
+        
+    });
+    
+router.route('/:id')
+    // GET a task by id endpoint
+    .get((req, res) => {
+
+    })
+
+    // UPDATE a task by id endpoint
+    .put((req, res) => {
+
+    })
+
+    // DELETE a task by id endpoint
+    .delete((req, res) => {
+
+    });
+
+    
+        
+
+export default router;
+
