7bb7186122a2e18ccd6dc95baaee6e2bf7ad89e0
Anel Canto <ac@anelcanto.com>
2023-05-20 04:47:50 -1000

    Update so that all RESTful routes are included

--- routes/tasks.js
+++ routes/tasks.js
@@ -12,6 +12,15 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        res.json(task);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 router.post('/', async (req, res) => {
     const task = new Task({
         title: req.body.title,
@@ -27,6 +36,32 @@ router.post('/', async (req, res) => {
         res.status(201).json(newTask);
     } catch (err) {
         res.status(400).json({ message: err.message });
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        task.title = req.body.title;
+        task.completed = req.body.completed;
+        task.description = req.body.description;
+        task.due_date = req.body.due_date;
+        task.priority = req.body.priority;
+        task.labels = req.body.labels;
+        const updatedTask = await task.save();
+        res.json(updatedTask);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        const deletedTask = await task.remove();
+        res.json(deletedTask);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
     }
 });
 
