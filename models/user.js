c57174791ad120f1424f106af5a0dd9b10ca301e
Anel Canto <ac@anelcanto.com>
2023-06-02 19:24:05 -1000

    Add users router

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const userSchema = new Mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        maxlength: 20,
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 20
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    first_name:{
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
+    },
+    last_name:{
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 20
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+}, { timestamps: true });
+
+export default Mongoose.model('User', userSchema);
---
+++ routes/users.js
@@ -0,0 +1,68 @@
+import Router from 'express';
+import User from '../models/user.js';
+
+const router = Router();
+
+router.route('/users/')
+    .get(async (req, res) => {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    })
+    .post(async (req, res) => {
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name
+        });
+
+        try {
+            const newUser = await user.save();
+            res.status(201).json(newUser);
+        } catch (err) {
+            res.status(400).json({ message: err.message });
+        }
+    });
+
+router.route('/users/:id')
+    .get(async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    })
+    .patch(async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            user.username = req.body.username;
+            user.password = req.body.password;
+            user.email = req.body.email;
+            user.first_name = req.body.first_name;
+            user.last_name = req.body.last_name;
+            const updatedUser = await user.save();
+            res.json(updatedUser);
+        } catch (err) { 
+            res.status(400).json({ message: err.message });
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            const deletedUser = await user.deleteOne()
+            res.json(deletedUser);
+        } catch (err) {
+            res.status(500).json({ message: err.message });
+        }
+    });
+
+export default router;
+
+
+
