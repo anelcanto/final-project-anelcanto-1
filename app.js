6b220380de30aeeedceb2a96cdb727e90a2b2209
Anel Canto <ac@anelcanto.com>
2023-06-07 03:47:29 -1000

    Fix taskroutes

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import tasksRouter from "./routes/tasks.js";
-import signupRoute from "./routes/signup.js";
+// import signupRoute from "./routes/signup.js";
 
-const URI = process.env.MONGO_URI || "mongodb://localhost:27017/tasks_db"; 
+const URI = process.env.MONGO_URI || "mongodb://localhost:27017/tasks_db";
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error))
@@ -18,9 +18,9 @@ db.once('open', () => {
     console.log('Connected to MongoDB!') // Log message once db opens
     const app = express() // Create express app
     app.use(express.json()) // Middleware for parsing JSON
-    
+
     app.use('/api/tasks', tasksRouter);
-    app.use('/api/signup', signupRoute); 
+    // app.use('/api/signup', signupRoute); 
 
     app.listen(PORT, () => console.log('server started')) // Start server
 })
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import Router from "express";
+import taskRouter from "./tasks.js";
+
+
+// const default = taskRouter.route('/')
+
+
+
+export default router;
