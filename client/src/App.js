28e2018906866a3a617daa1e6545f941d860b247
Anel Canto <ac@anelcanto.com>
2023-06-21 03:56:14 -1000

    Add navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './components/TodoList/TodoList';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/entities">Entities</Link>
+        </nav>
+        <Switch>
+          <Route path="/signup" />
+          <Route path="/login" />
+          <Route path="/tasks" />
+          <Route path="/" />
+        </Switch>
+      </Router>
+
+      <TodoList />
+
     </div>
   );
 }
